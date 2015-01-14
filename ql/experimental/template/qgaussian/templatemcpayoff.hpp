4cc4a8a179193bc4fb6c79eef001b33d97c0bf87
Sebastian Schlenkrich <sebastian.schlenkrich@d-fine.de>
2015-01-14 18:50:05 +0100

    swaption smile

--- ql/experimental/template/qgaussian/templatemcpayoff.hpp
+++ ql/experimental/template/qgaussian/templatemcpayoff.hpp
@@ -42,10 +42,27 @@ namespace QuantLib {
 			std::vector< boost::shared_ptr<TemplateMCPayoff> > payoffs_;
 			boost::shared_ptr<SimulationType>                  simulation_;
 		public:
+
+			inline static ActiveType NPV(const std::vector< boost::shared_ptr<TemplateMCPayoff> >& payoffs, 
+				                         const boost::shared_ptr<SimulationType>&                  simulation) {
+				ActiveType npv = 0.0;
+				for (size_t k=0; k<payoffs.size(); ++k) {
+					ActiveType npvk = 0;
+					for (size_t n=0; n<simulation->nPaths(); ++n) {
+						npvk += payoffs[k]->discountedAt(simulation->path(n));
+					}
+					npv += npvk;
+				}
+				return npv / simulation->nPaths();
+			}
+
+
 			Pricer (const std::vector< boost::shared_ptr<TemplateMCPayoff> >& payoffs, 
 				    const boost::shared_ptr<SimulationType>&                  simulation)
 					: payoffs_(payoffs), simulation_(simulation) { }
+
 			inline ActiveType NPV() {
+				/*
 				ActiveType npv = 0.0;
 				for (size_t k=0; k<payoffs_.size(); ++k) {
 					ActiveType npvk = 0;
@@ -55,6 +72,8 @@ namespace QuantLib {
 					npv += npvk;
 				}
 				return npv / simulation_->nPaths();
+				*/
+				return NPV(payoffs_,simulation_);
 			}
 		};
 
@@ -68,8 +87,70 @@ namespace QuantLib {
 		public:
 			Cash( DateType obsTime, DateType payTime ) : TemplateMCPayoff(obsTime), payTime_(payTime) { }
 			virtual ActiveType at(const boost::shared_ptr<PathType>& p) {
-				if (payTime_==observationTime()) return (ActiveType)1.0;
+				if (payTime_<=observationTime()) return (ActiveType)1.0;
 				return p->zeroBond(observationTime(),payTime_);
+			}
+		};
+
+		// annuity
+		class Annuity : public TemplateMCPayoff {
+		protected:
+			std::vector<DateType>    payTimes_;
+			std::vector<PassiveType> payWeights_;  // these are typically year fractions
+		public:
+			Annuity( DateType                        obsTime, 
+				     const std::vector<DateType>&    payTimes,
+				     const std::vector<PassiveType>& payWeights)
+				: TemplateMCPayoff(obsTime), payTimes_(payTimes), payWeights_(payWeights) { }
+			virtual ActiveType at(const boost::shared_ptr<PathType>& p) {
+				ActiveType res = 0.0;
+				size_t N = (payTimes_.size()<payWeights_.size()) ? (payTimes_.size()) : (payWeights_.size());
+				for (size_t k=0; k<N; ++k) {
+					if (payTimes_[k]>observationTime()) {
+						res += payWeights_[k] * p->zeroBond(observationTime(),payTimes_[k]);
+					}
+				}
+				return res;
+			}
+		};
+
+		// prototypical physically settled European swaption
+		class ModelSwaption : public TemplateMCPayoff {
+		protected:
+			std::vector<DateType>    times_;        // T_0, .., T_N
+			std::vector<PassiveType> payWeights_;   // tau_0, .., tau_N-1
+			PassiveType              strikeRate_;   // option strike
+			PassiveType              payOrRec_;     // call (+1) or put (-1) option on swap rate
+			bool                     isConsistent_; // check consistency of input
+		public:
+			ModelSwaption( DateType                        obsTime, 
+				           const std::vector<DateType>&    times,
+				           const std::vector<PassiveType>& payWeights,
+					       PassiveType                     strikeRate,
+					       PassiveType                     payOrRec      )
+				: TemplateMCPayoff(obsTime), times_(times), payWeights_(payWeights), strikeRate_(strikeRate), payOrRec_(payOrRec) { 
+				isConsistent_ = true;
+				if (times_.size()<2) isConsistent_ = false;
+				for (size_t k=0; k<times_.size(); ++k) if (times_[k]<observationTime()) isConsistent_ = false;
+				// default weights
+				if (payWeights_.size()!=times_.size()-1) {
+					payWeights_.resize(times_.size()-1);
+					for (size_t k=0; k<payWeights_.size(); ++k) payWeights_[k] = times_[k+1] - times_[k];
+				}
+				// finished
+			}
+			virtual ActiveType at(const boost::shared_ptr<PathType>& p) {
+				ActiveType res = 0.0;
+				if (!isConsistent_) return res;
+				// annuity...
+				for (size_t k=0; k<payWeights_.size(); ++k) res += payWeights_[k] * p->zeroBond(observationTime(),times_[k+1]);
+				// floatleg - fixedleg...
+				res = p->zeroBond(observationTime(),times_[0]) - p->zeroBond(observationTime(),times_[times_.size()-1]) - strikeRate_*res;
+				// payer or receiver swap...
+				res *= payOrRec_;
+				// exercise option...
+				res = (res>0) ? res : 0.0;
+				return res;
 			}
 		};
 
