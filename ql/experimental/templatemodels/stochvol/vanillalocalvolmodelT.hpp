c81638f413a2f8920634d4d46ea3b42370afbb39
Sebastian Schlenkrich <sebastian.schlenkrich@d-fine.de>
2017-11-30 14:26:13 +0100

    ATM calibration added

--- ql/experimental/templatemodels/stochvol/vanillalocalvolmodelT.hpp
+++ ql/experimental/templatemodels/stochvol/vanillalocalvolmodelT.hpp
@@ -11,6 +11,9 @@
 #define quantlib_templatevanillalocalvolmodel_hpp
 
 #include <vector>
+#include <string>
+
+#include <stdio.h>
 
 #include <boost/shared_ptr.hpp>
 #include <ql/errors.hpp>
@@ -43,13 +46,20 @@ namespace QuantLib {
 		PassiveType              alpha_;   // out-of-the-model adjuster for straddle
 		PassiveType              nu_;      // out-of-the-model adjuster for forward
 		// numerical accuracy parameters (maybe expose to user...)
-		PassiveType              extrapolationStdevs_;  // number of stdevs used as lower and upper cutoff
+		PassiveType              extrapolationStdevs_;  // number of stdevs used as lower and upper cutoff, default 10
+		size_t                   maxCalibrationIters_;  // number of iterations for forward/sigma0 calibration
+		PassiveType              sigma0Tol_;            // tolerance for sigma convergence
+		PassiveType              S0Tol_;                // tolerance for forward convergence
+		bool                     adjustATM_;            // apply post-calibration ATM adjuster
+		// we may want some debug information for the calibration process
+		bool                     enableLogging_;
+		std::vector<std::string> logging_;
  
 		// determine the lower and upper bounds for integration
 		inline PassiveType lowerBoundX() { return  -extrapolationStdevs_ * sqrt(T_) + mu_; }
 		inline PassiveType upperBoundX() { return   extrapolationStdevs_ * sqrt(T_) + mu_; }		
 
-		PassiveType localVol(const bool isRightWing, const size_t k, const PassiveType S) {
+		inline PassiveType localVol(const bool isRightWing, const size_t k, const PassiveType S) {
 			// this is an unsafe method specifying the vol function sigma(S) on the individual segments
 			if (isRightWing) {
 				QL_REQUIRE(k < sigmaP_.size(), "k < sigmaP_.size() required.");
@@ -65,7 +75,7 @@ namespace QuantLib {
 			return 0.0;  // this should never be reached
 		}
 
-		PassiveType underlyingS(const bool isRightWing, const size_t k, const PassiveType x) {
+		inline PassiveType underlyingS(const bool isRightWing, const size_t k, const PassiveType x) {
 			// this is an unsafe method specifying the underlying level S(x) on the individual segments
 			if (isRightWing) {
 				QL_REQUIRE(k < sigmaP_.size(), "k < sigmaP_.size() required.");
@@ -86,7 +96,7 @@ namespace QuantLib {
 			return 0.0;  // this should never be reached
 		}
 
-		PassiveType underlyingX(const bool isRightWing, const size_t k, const PassiveType S) {
+		inline PassiveType underlyingX(const bool isRightWing, const size_t k, const PassiveType S) {
 			// this is an unsafe method specifying the underlying level x(S) on the individual segments
 			if (isRightWing) {
 				QL_REQUIRE(k < sigmaP_.size(), "k < sigmaP_.size() required.");
@@ -109,7 +119,7 @@ namespace QuantLib {
 			return 0.0;  // this should never be reached
 		}
 
-		PassiveType primitiveF(const bool isRightWing, const size_t k, const PassiveType x) {
+		inline PassiveType primitiveF(const bool isRightWing, const size_t k, const PassiveType x) {
 			// this is an unsafe method specifying the primitive function F(x) = \int [alpha S(x) + nu] p(x) dx
 			// on the individual segments
 			PassiveType sigma0, x0, S0, m0;
@@ -144,10 +154,9 @@ namespace QuantLib {
 			return alpha_ * (term1 - term2);
 		}
 
-		void updateLocalVol() {
+		inline void updateLocalVol() {
 			// use ODE solution to determine x-grid and sigma-grid taking into account constraints of
 			// positive local volatility and local vol extrapolation
-			mu_ = - (Mm_[0] + Mp_[0]) / 4.0 * T_; // this should be exact for shifted log-normal models
 			for (size_t k = 0; k < Sp_.size(); ++k) { // right wing calculations
 				PassiveType x0 = (k > 0) ? Xp_[k - 1] : 0.0;
 				PassiveType S0 = (k > 0) ? Sp_[k - 1] : S0_;
@@ -166,7 +175,6 @@ namespace QuantLib {
 				QL_REQUIRE(sigmaP_[k] > 0.0, "sigmaP_[k] > 0.0 required.");
 				Xp_[k] = underlyingX(true, k, Sp_[k]);
 			}
-
 			for (size_t k = 0; k < Sm_.size(); ++k) { // left wing calculations
 				PassiveType x0 = (k > 0) ? Xm_[k - 1] : 0.0;
 				PassiveType S0 = (k > 0) ? Sm_[k - 1] : S0_;
@@ -187,6 +195,61 @@ namespace QuantLib {
 			}
 		}
 
+		inline void calibrateATM() {
+			PassiveType straddleVega = straddleATM_ / sigmaATM_;
+			PassiveType forwardMinusStrike0, forwardMinusStrike1, straddleMinusATM0, straddleMinusATM1, dmu, dsigma0;
+			PassiveType	dfwd_dmu, dstr_dsi;
+			for (size_t k = 0; k < maxCalibrationIters_; ++k) {
+				PassiveType call = expectation(true, S0_);
+				PassiveType put = expectation(false, S0_);
+				forwardMinusStrike1 = call - put;
+				straddleMinusATM1 = call + put - straddleATM_;
+				if (k == 0) {
+					dfwd_dmu = sigma0_;        // this is an estimate based on dS/dX at ATM
+					dstr_dsi = straddleVega;   // this is an estimate based on dsigmaATM / dsigma0 =~ 1
+				}
+				if (k>0) { // we use secant if available
+					// only update derivative if we had a step, otherwise use from previous iteration
+					if (fabs(dmu)>1.0e-16) dfwd_dmu = (forwardMinusStrike1 - forwardMinusStrike0) / dmu;
+					if (fabs(dsigma0)>1.0e-16) dstr_dsi = (straddleMinusATM1 - straddleMinusATM0) / dsigma0;
+				}
+				dmu = -forwardMinusStrike1 / dfwd_dmu;
+				dsigma0 = -straddleMinusATM1 / dstr_dsi;
+				if ((sigma0_ + dsigma0) < 0.0) dsigma0 = -0.5 * sigma0_;  // make sure sigma0_ remains positive
+				// maybe some line search could improve convergence...
+				mu_ += dmu;
+				sigma0_ += dsigma0;
+				updateLocalVol();
+				// prepare for next iteration
+				forwardMinusStrike0 = forwardMinusStrike1;
+				straddleMinusATM0 = straddleMinusATM1;
+				if (enableLogging_) logging_.push_back("k: " + std::to_string(k) +
+					"; C: " + std::to_string(call) +
+					"; P: " + std::to_string(put) +
+					"; S: " + std::to_string(straddleATM_) +
+					"; dfwd_dmu: " + std::to_string(dfwd_dmu) +
+					"; dstr_dsi: " + std::to_string(dstr_dsi) +
+					"; dmu: " + std::to_string(dmu) +
+					"; dsigma0: " + std::to_string(dsigma0));
+				if ((fabs(forwardMinusStrike0) < S0Tol_) && (fabs(dsigma0) < sigma0Tol_)) break;
+			}
+		}
+
+		inline void adjustATM() {
+			// reset adjusters in case this method is invoked twice
+			alpha_ = 1.0;
+			nu_ = 0.0;
+			PassiveType call0 = expectation(true, S0_);
+			PassiveType put0 = expectation(false, S0_);
+			nu_ = put0 - call0;
+			if (enableLogging_) logging_.push_back("C0: " + std::to_string(call0) + "; P0: " + std::to_string(put0) + "; nu: " + std::to_string(nu_));
+			PassiveType call1 = expectation(true, S0_);
+			PassiveType put1 = expectation(false, S0_);
+			alpha_ = straddleATM_ / (call1 + put1);
+			nu_ = alpha_*nu_ + (1.0 - alpha_)*S0_;
+			if (enableLogging_) logging_.push_back("C1: " + std::to_string(call1) + "; P1: " + std::to_string(put1) + "; alpha_: " + std::to_string(alpha_) + "; nu_: " + std::to_string(nu_));
+		}
+
 	public:
 		VanillaLocalVolModelT(
 			const DateType                   T,
@@ -195,11 +258,16 @@ namespace QuantLib {
 			const std::vector<PassiveType>&  Sp,
 			const std::vector<PassiveType>&  Sm,
 			const std::vector<PassiveType>&  Mp,
-			const std::vector<PassiveType>&  Mm )
-			: T_(T), S0_(S0), sigmaATM_(sigmaATM), sigma0_(sigmaATM), Sp_(Sp), Sm_(Sm), Mp_(Mp), Mm_(Mm), mu_(0.0), alpha_(1.0), nu_(0.0), extrapolationStdevs_(10.0) {
+			const std::vector<PassiveType>&  Mm,
+			// controls for calibration
+			const size_t                     maxCalibrationIters = 5,
+			bool                             adjustATMFlag = true,
+			bool                             enableLogging = false	)
+			: T_(T), S0_(S0), sigmaATM_(sigmaATM), Sp_(Sp), Sm_(Sm), Mp_(Mp), Mm_(Mm),
+			maxCalibrationIters_(maxCalibrationIters), adjustATM_(adjustATMFlag), enableLogging_(enableLogging) {
 			// some basic sanity checks come here to avoid the need for taking care of it later on
 			QL_REQUIRE(T_ > 0, "T_ > 0 required.");
-			QL_REQUIRE(sigma0_ > 0, "sigma0_ > 0 required.");
+			QL_REQUIRE(sigmaATM_ > 0, "sigmaATM_ > 0 required.");
 			QL_REQUIRE(Sp_.size() > 0, "Sp_.size() > 0 required.");
 			QL_REQUIRE(Sm_.size() > 0, "Sm_.size() > 0 required.");
 			QL_REQUIRE(Mp_.size() == Sp_.size(), "Mp_.size() == Sp_.size() required.");
@@ -214,11 +282,25 @@ namespace QuantLib {
 			sigmaM_.resize(Sm_.size());
 			Xp_.resize(Sp_.size());
 			Xm_.resize(Sm_.size());
-			// now we may calculate local volatility
+			// initialize deep-in-the-model parameters
+			straddleATM_ = sigmaATM_ * sqrt(T_) * M_1_SQRTPI * M_SQRT_2 * 2.0;
+			sigma0_ = sigmaATM_;
+			mu_     = -(Mm_[0] + Mp_[0]) / 4.0 * T_; // this should be exact for shifted log-normal models
+			alpha_  = 1.0;
+			nu_ =     0.0;
+			extrapolationStdevs_ = 10.0;
+			sigma0Tol_           = 1.0e-12;
+			S0Tol_               = 1.0e-12;
+		    // now we may calculate local volatility
 			updateLocalVol();
+			calibrateATM();
+			if (adjustATM_) adjustATM();
 		}
 
 		// inspectors
+
+		const std::vector<std::string> logging() { return logging_; }
+
 		const std::vector<PassiveType> underlyingX() {
 			std::vector<PassiveType> X(Xm_.size() + Xp_.size() + 1);
 			for (size_t k = 0; k < Xm_.size(); ++k) X[k] = Xm_[Xm_.size() - k - 1];
@@ -308,8 +390,8 @@ namespace QuantLib {
 				PassiveType intK = TemplateAuxilliaries::Phi((xEnd - mu_) / sqrt(T_)) - TemplateAuxilliaries::Phi((strikeX - mu_) / sqrt(T_));
 				return intS - strike * intK;
 			}
-
 		}
+
 
 	};
 
